49037fc5ae0cadc0b8df31f24a1ee9b2ea13875c
DVSR Bharadwaj <dvsrbharadwaj11@gmail.com>
2025-11-22 20:01:47 +0530

    Hyperparameter Tuning

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,12 +41,83 @@ class ModelTrainer:
                 'Linear Regression': LinearRegression(),
                 'K-Neighbors Classifier': KNeighborsRegressor(),
                 'XGBClassifier': XGBRegressor(),
-                'CatBoosting Classifier': CatBoostRegressor(verbose=False),
+                'CatBoosting Classifier': CatBoostRegressor(random_state=42, 
+                                                            #verbose=False, 
+                                                            logging_level='Silent',
+                                                            thread_count=-1),
                 'AdaBoost Classifier': AdaBoostRegressor()
+            }
+            param_grids = {
+                "Random Forest": {
+                    "n_estimators": [300, 400, 500, 600],
+                    "max_depth": [6, 7, 8, 9, 10, None],
+                    #"min_samples_split": [2, 5, 10],
+                    #"min_samples_leaf": [1, 2, 4],
+                    #"max_features": ["sqrt", "log2", 0.7, 0.8],
+                },
+
+                "Gradient Boosting": {
+                    "n_estimators": [300, 400, 500, 600],
+                    "learning_rate": [0.01, 0.03, 0.05, 0.08],
+                    #"max_depth": [3, 4, 5, 6],
+                    #"min_samples_split": [2, 5, 10],
+                    #"min_samples_leaf": [1, 2, 4],
+                    #"subsample": [0.7, 0.8, 0.9, 1.0],
+                },
+
+                "XGBClassifier": {
+                    "n_estimators": [400, 500, 600, 700, 800],
+                    "learning_rate": [0.01, 0.02, 0.03, 0.05, 0.08],
+                    #"max_depth": [4, 5, 6, 7],
+                    #"min_child_weight": [1, 3, 5],
+                    #"subsample": [0.7, 0.8, 0.9, 1.0],
+                    #"colsample_bytree": [0.7, 0.8, 0.9, 1.0],
+                    #"gamma": [0, 0.1, 0.2],
+                    #"reg_alpha": [0, 0.1, 0.5],
+                    #"reg_lambda": [0.5, 1.0, 1.5],
+                },
+
+                "CatBoosting Classifier": {
+                    "iterations": [500, 700, 800, 1000],
+                    "learning_rate": [0.01, 0.03, 0.05, 0.07],
+                    #"depth": [4, 5, 6, 7, 8],
+                    #"l2_leaf_reg": [1, 3, 5, 7],
+                    #"border_count": [128, 254],
+                    #"bagging_temperature": [0.0, 0.2, 0.5, 1.0],
+                    #"random_strength": [0.5, 1, 2],
+                },
+
+                "K-Neighbors Classifier": {
+                    "n_neighbors": [5, 7, 9, 11, 13, 15],
+                    "weights": ["uniform", "distance"],
+                    #"metric": ["euclidean", "manhattan", "minkowski"],
+                    #"p": [1, 2]  # only used if metric='minkowski'
+                },
+
+                "AdaBoost Classifier": {
+                    "n_estimators": [200, 300, 400, 500],
+                    "learning_rate": [0.05, 0.1, 0.3, 0.5, 1.0],
+                    #"loss": ["linear", "square", "exponential"],
+                    #"base_estimator": [
+                        #DecisionTreeRegressor(max_depth=3),
+                       # DecisionTreeRegressor(max_depth=4),
+                        #DecisionTreeRegressor(max_depth=5)
+                    #]
+                },
+
+                "Decision Tree": {
+                    "max_depth": [4, 5, 6, 7, 8, 10, None],
+                    "min_samples_split": [2, 5, 10, 20],
+                    #"min_samples_leaf": [1, 2, 4, 8],
+                    #"max_features": ["sqrt", "log2", None]
+                },
+
+                # LinearRegression has no hyperparameters to tune
+                "Linear Regression": {}
             }
 
             model_report: dict = evaluate_models(x_train=x_train, y_train=y_train,
-                                                x_test=x_test, y_test=y_test, models=models)
+                                                x_test=x_test, y_test=y_test, models=models, param=param_grids)
             # To get the best model score 
             best_model_score = max(sorted(model_report.values()))
 
